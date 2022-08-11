6cb38520547affe47107252490cd4d61866f8cef
vezz <mati.majewski@wp.pl>
2022-08-11 18:10:01 +0200

    removed cors

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express')
-// const cors = require('cors')
 const morgan = require('morgan')
 const router = require('./routes/cards.js')
-
-// TODO add proper logger middleware
-
 const app = express()
+
 
 app.set('view engine', 'ejs');
 app.use(morgan('dev'))
 app.use('/cards', router)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// app.use(cors({ origin: "*" }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
